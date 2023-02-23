b8e8bd2800d81add8384e82b9cbc43dba8086285
Digvijay Chauhan <virender@codedrillinfotech.com>
2023-02-24 00:10:01 +0530

    s

--- server.js
+++ server.js
@@ -15,17 +15,18 @@ const clientMessage = "successfully registered our team will contact you soon";
 const mdMessage = "New User";
 app.post("/formSubmit", async (req, res) => {
   try {
+    console.log(req.body)
     if (req.body) {
       const storeData = await Users.create(req.body);
       if (storeData) {
-        const sendMail = await smtp(process.env.SENDGRID_API_KEY, {
-          sendTo: storeData.mail,
-          message: clientMessage,
-        });
-        const sendMailToMD = await smtp(process.env.SENDGRID_API_KEY, {
-          sendTo: "drana5338@gmail.com",
-          message: mdMessage,
-        });
+        // const sendMail = await smtp(process.env.SENDGRID_API_KEY, {
+        //   sendTo: storeData.mail,
+        //   message: clientMessage,
+        // });
+        // const sendMailToMD = await smtp(process.env.SENDGRID_API_KEY, {
+        //   sendTo: "drana5338@gmail.com",
+        //   message: mdMessage,
+        // });
         res.send({ data: "success" });
       } else {
         res.send({ error: "Something went wrong" });
